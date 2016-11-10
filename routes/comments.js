6d39d962f26b7b7b7a4e63305335d999f0ee110a
GeorgeFourikis <georgefourikis@hotmail.gr>
2016-11-10 19:56:50 +0200

    Finished CRUD after insertind DELETE method on comments

--- routes/comments.js
+++ routes/comments.js
@@ -65,6 +65,17 @@ router.put("/:comment_id", function(req, res){
   });
 });
 
+router.delete("/:comment_id", function(req, res){
+  //find by id and destroy
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    }else{
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 
 // middleware
 function isLoggedIn(req, res, next){
