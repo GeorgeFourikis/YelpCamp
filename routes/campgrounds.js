a5dcc5b301240accae95ebe029118a5b7b4db2c8
GeorgeFourikis <georgefourikis@hotmail.gr>
2016-11-10 18:19:23 +0200

    Added Authorization on Edit-Update-Destroy functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,18 +57,14 @@ router.get("/:id", function(req, res){
 });
 
 //Edit Campground Route
-router.get("/:id/edit", function(req, res){
-  Campground.findById(req.params.id, function(err, foundCampground){
-    if(err){
-      res.redirect("/campgrounds");
-    } else {
-      res.render("campgrounds/edit", {campground: foundCampground});
-    }
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+          res.render("campgrounds/edit", {campground: foundCampground});
+    });
   });
-});
 
 //Update Campground Route
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership,  function(req, res){
   //find and update the correct Campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
     if(err){
@@ -82,7 +78,7 @@ router.put("/:id", function(req, res){
 
 
 // Destroy Campground Route
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership,  function(req, res){
   Campground.findByIdAndRemove(req.params.id, function(err){
     if(err){
       res.redirect("/campgrounds");
@@ -99,6 +95,25 @@ function isLoggedIn(req, res, next){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+        res.redirect("/campgrounds");
+      } else {
+        //does user own the Camground?
+        if(foundCampground.author.id.equals(req.user._id)){
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 // exporting the router always
